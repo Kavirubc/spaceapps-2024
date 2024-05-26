ab870d136fd21a04ce7d50cc1d43d17915df5665
kaviru <hapuarachchikaviru@gmail.com>
2024-05-27 00:31:59 +0530

    particles added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -3,10 +3,12 @@ import React from "react";
 import Image from 'next/image'
 
 import logo from '@/../public/logos/bannerlogo_2022.png'
+import ParticleDesign from "./particles";
 
 const HeroSec: React.FC = () => {
     return (
         <main className="flex flex-col items-center justify-center min-h-screen bg-cover bg-center text-white text-center" style={{ backgroundImage: "url('/path/to/your/background-image.png')" }}>
+            <ParticleDesign />
             <div className="flex flex-col items-center space-y-4">
                 <div className="flex flex-row gap-x-2">
                     <Image className="lg:max-w-md max-w-xs" src={logo} alt="Space Apps Challenge 2024" />
